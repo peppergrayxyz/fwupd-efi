5b410cc73be2ce3aa57b9318ff044d514f3fe4cd
Pepper Gray <111446242+peppergrayxyz@users.noreply.github.com>
2025-07-10 23:39:28 +0200

    .

--- contrib/ci.sh
+++ contrib/ci.sh
@@ -4,52 +4,58 @@ shopt -s extglob
 rm -rf build/
 
 # disable the safe directory feature
-if command -v git; then git config --global safe.directory "*"; fi
-
-# shellcheck source=/dev/null
-echo "building on $(source "/etc/os-release" && echo "$PRETTY_NAME")"
+if command -v git > /dev/null 2>&1; then 
+    git config --global safe.directory "*"; 
+fi
 
 set -x
-case "$CI_DISTRO" in
-    fedora*)
-        meson build
-        VERSION=`meson introspect build --projectinfo | jq -r .version`
-        RPMVERSION=${VERSION//-/.}
-        sed "s,#VERSION#,$RPMVERSION,;
-            s,#BUILD#,1,;
-            s,#LONGDATE#,`date '+%a %b %d %Y'`,;
-            s,#ALPHATAG#,alpha,;
-            s,Source0.*,Source0:\tfwupd-efi-$VERSION.tar.xz," \
-            contrib/fwupd-efi.spec.in > build/fwupd-efi.spec
-        if [ -n "$CI" ]; then
-            sed -i "s,enable_ci 0,enable_ci 1,;" build/fwupd-efi.spec
-        fi
-        ninja -C build dist
-        mkdir -p $HOME/rpmbuild/SOURCES/
-        mv build/meson-dist/fwupd-efi-$VERSION.tar.xz $HOME/rpmbuild/SOURCES/
-        rpmbuild -ba build/fwupd-efi.spec
-        mkdir -p dist
-        cp $HOME/rpmbuild/RPMS/*/*.rpm dist
-    ;;
-    debian*)
-        export DEBFULLNAME="CI Builder"
-        export DEBEMAIL="ci@travis-ci.org"
-        VERSION=`head meson.build | grep ' version :' | cut -d \' -f2`
-        mkdir -p build
-        cp -lR !(build|dist|venv) build/
-        pushd build
-        mv contrib/debian .
-        sed s/quilt/native/ debian/source/format -i
-        #build the package
-        EDITOR=/bin/true dch --create --package fwupd-efi -v $VERSION "CI Build"
-        debuild --no-lintian --preserve-envvar CI --preserve-envvar CC \
-            --preserve-envvar QUBES_OPTION
-    ;;
-    alpine*)
-        case "$CI_DISTRO" in alpine-llvm*) export CC=clang CCLD=lld ;; esac
-    ;&
-    *)
+# Build
+if [ -z "$CI_BUILDPKG" ]; then
+    # normal build
+    if [ -n "$LLVM" ]; then export CC=clang CC_LD=lld CFLAGS='-fuse-ld=lld' LDFLAGS='-fuse-ld=lld' AR=llvm-ar STRIP=llvm-strip; fi
+    # OBJCOPY=llvm-objcopy
     meson build
     ninja -C build
-    ;;
-esac
+else
+    # package build
+    case "$CI_DISTRO" in
+        fedora*)
+            meson build
+            VERSION=`meson introspect build --projectinfo | jq -r .version`
+            RPMVERSION=${VERSION//-/.}
+            sed "s,#VERSION#,$RPMVERSION,;
+                s,#BUILD#,1,;
+                s,#LONGDATE#,`date '+%a %b %d %Y'`,;
+                s,#ALPHATAG#,alpha,;
+                s,Source0.*,Source0:\tfwupd-efi-$VERSION.tar.xz," \
+                contrib/fwupd-efi.spec.in > build/fwupd-efi.spec
+            if [ -n "$CI" ]; then
+                sed -i "s,enable_ci 0,enable_ci 1,;" build/fwupd-efi.spec
+            fi
+            ninja -C build dist
+            mkdir -p $HOME/rpmbuild/SOURCES/
+            mv build/meson-dist/fwupd-efi-$VERSION.tar.xz $HOME/rpmbuild/SOURCES/
+            rpmbuild -ba build/fwupd-efi.spec
+            mkdir -p dist
+            cp $HOME/rpmbuild/RPMS/*/*.rpm dist
+        ;;
+        debian*)
+            export DEBFULLNAME="CI Builder"
+            export DEBEMAIL="ci@travis-ci.org"
+            VERSION=`head meson.build | grep ' version :' | cut -d \' -f2`
+            mkdir -p build
+            cp -lR !(build|dist|venv) build/
+            pushd build
+            mv contrib/debian .
+            sed s/quilt/native/ debian/source/format -i
+            #build the package
+            EDITOR=/bin/true dch --create --package fwupd-efi -v $VERSION "CI Build"
+            debuild --no-lintian --preserve-envvar CI --preserve-envvar CC \
+                --preserve-envvar QUBES_OPTION
+        ;;
+        *)
+            echo "$0: unsupported distribution '$CI_DISTRO'" >&2
+            exit 1
+        ;;
+    esac
+fi
--- contrib/ci_install.sh
+++ contrib/ci_install.sh
@@ -1,42 +1,45 @@
 #!/bin/sh
-set -exuo
+set -e
+
+if [ -z "$CI_DISTRO" ]; then
+  if   command -v apt    > /dev/null 2>&1; then CI_DISTRO='debian'; 
+  elif command -v dnf    > /dev/null 2>&1; then CI_DISTRO='fedora'; 
+  elif command -v pacman > /dev/null 2>&1; then CI_DISTRO='arch'; 
+  elif command -v apk    > /dev/null 2>&1; then CI_DISTRO='alpine'; 
+  elif command -v emerge > /dev/null 2>&1; then CI_DISTRO='gentoo'; 
+  fi
+fi
+
+echo "Installing packages for $CI_DISTRO:" 
 
 case "$CI_DISTRO" in
   ubuntu*|debian*)
-    apt-get update -y
-    apt-get install -y build-essential cmake pkg-config python3 ninja-build meson
-    apt-get install -y python3-pefile gnu-efi
-    if case "$CI_DISTRO" in debian*) ;; *) false;; esac; then
-        apt-get install -y devscripts mingw-w64-tools gobject-introspection
-    fi
+    apt update -y
+    apt install -y build-essential cmake pkg-config python3 ninja-build meson
+    apt install -y python3-pefile gnu-efi
+    [ -n "$LLVM" ]        && apt install -y llvm clang lld
+    [ -n "$CI_BUILDPKG" ] && apt install -y devscripts mingw-w64-tools gobject-introspection
     ;;
 
   fedora*)
     dnf -y update
     dnf -y install make automake gcc python3 ninja-build meson
     dnf -y install python3-pefile gnu-efi-devel
-    dnf -y install git jq fedora-packager rpmdevtools pesign
+    [ -n "$LLVM" ]        && dnf -y install llvm clang lld
+    [ -n "$CI_BUILDPKG" ] && dnf -y install git jq fedora-packager rpmdevtools pesign
     ;;
 
   arch*)
     pacman -Sy --noconfirm archlinux-keyring
     pacman -Sy --noconfirm base-devel python3 ninja meson
+    [ -n "$LLVM" ] && pacman -Sy --noconfirm llvm clang lld
     pacman -Sy --noconfirm python-pefile gnu-efi
-    ;;
-
-  gentoo*)
-    echo ". /etc/profile" > /root/.bashrc
-    emerge-webrsync --quiet
-    emerge --quiet dev-python/pefile sys-boot/gnu-efi
     ;;
 
   alpine*)
     apk update
-    apk add bash meson mingw-w64-tools
-    case "$CI_DISTRO" in
-      alpine-gcc*)  apk add build-base ;;
-      alpine-llvm*) apk add clang lld  ;;
-    esac
+    apk add bash meson build-base
+    [ -n "$LLVM" ] && apk add llvm clang lld
     apk add py3-pefile gnu-efi-dev
     ;;
 
